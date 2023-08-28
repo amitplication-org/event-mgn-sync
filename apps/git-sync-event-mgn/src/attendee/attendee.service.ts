cb0d9f15329503cfa64a8f8c17b54c9c40174644
Amit Barletz <amit@amplication.com>
2023-08-28 15:32:52 +0300

    custom change

--- apps/git-sync-event-mgn/src/attendee/attendee.service.ts
+++ apps/git-sync-event-mgn/src/attendee/attendee.service.ts
@@ -7,4 +7,18 @@ export class AttendeeService extends AttendeeServiceBase {
   constructor(protected readonly prisma: PrismaService) {
     super(prisma);
   }
+  
+  async greetingAttendee(attendeeId: string): Promise<string> {
+    const attendee = await this.prisma.attendee.findUnique({
+      where: {
+        id: attendeeId,
+      },
+    });
+
+    if (!attendee) {
+      throw new Error("Attendee not found");
+    }
+
+    return `Hello ${attendee.name}`;
+  }
 }
